286e53b254b90bdffd7bddcecf266d9ef173fbef
osama0210 <osama.asmi04@gmail.com>
2025-03-24 15:24:28 +0100

    Models files added started with curd

--- restaurant/routes/web.php
+++ restaurant/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/menu', [MenuController::class, 'index']);
