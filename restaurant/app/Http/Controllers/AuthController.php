ef69dbb6ebea741b5599e9fb9908d680ee4ab46b
osama0210 <osama.asmi04@gmail.com>
2025-04-15 11:27:17 +0200

    Started with login logic.

--- restaurant/app/Http/Controllers/AuthController.php
+++ restaurant/app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -9,6 +10,22 @@ class AuthController extends Controller
     public function index(){
         return view('register');
     }
+
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return redirect('/login')->with('success', 'Registration Successful');
+    }
+
     public function showLoginForm(){
         return view('login');
     }
